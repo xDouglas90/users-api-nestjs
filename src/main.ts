db02ea39623b1a427f9a2b473ceb1876a44a592b
odouglas.dev <odouglas.dev@gmail.com>
2022-10-26 07:00:20 -0300

    :sparkles: feat: Add some necessary options to ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // only allow properties defined in the DTO
+      forbidNonWhitelisted: true, // throw an error if a property is not defined in the DTO
+      transform: true, // transform the payload to the DTO type
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
