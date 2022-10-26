f083f01accb0ab518d6ba7fe1a8bb826a81d6d51
odouglas.dev <odouglas.dev@gmail.com>
2022-10-26 06:42:59 -0300

    :sparkles: feat: Add DTO validations

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,12 @@
+import { IsArray, IsString } from 'class-validator';
+
 export class UpdateUserDto {
+  @IsString()
   readonly name?: string;
+
+  @IsString()
   readonly email?: string;
+
+  @IsArray()
+  readonly stack?: string[];
 }
